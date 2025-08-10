d2444e885652041a78152a163310d451df7fb124
Sagar Kadam <kadamsagar57@gmail.com>
2025-08-10 23:31:06 +0000

    its - Maze Kokan News Channel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -197,7 +197,7 @@ export default function Home() {
        <footer className="bg-background border-t border-border mt-8">
         <div className="container mx-auto py-4 px-4 text-center text-muted-foreground text-sm">
           <p>
-            Creator - <strong>Sagar Kadam</strong> | Made for 'Mazhe Kokan News Channel'.
+            Creator - <strong>Sagar Kadam</strong> | Made for 'Maze Kokan News Channel'.
           </p>
         </div>
       </footer>
